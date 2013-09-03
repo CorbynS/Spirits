0264620d3de5d2d56cafd26e7f765bfff1423103
Corbyn Salisbury <corbynsal@utexas.edu>
2013-09-03 09:35:51 -0500

    Added email links and other small changes

--- app/control/contact_controller.js
+++ app/control/contact_controller.js
@@ -3,66 +3,79 @@ angular.module("Spirits").controller("ContactCtrl", function($scope) {
 	    {
 	    	"name": "Tori Kasmierski",
 	    	"title": "Chief Haunt",
-	    	"pic": "img/contact/Tori.JPG"
+	    	"pic": "img/contact/Tori.JPG",
+	    	"email": "chiefhaunt13@gmail.com",
 	    },
 	    {
 	    	"name": "Nicole Martinez",
 	    	"title": "Vice Haunt",
 	    	"pic": "img/contact/Nicole.JPG",
+	    	"email": "vicehaunt13@gmail.com"
 	    },
 	    {
 	    	"name": "Caitlin Pawlowski",
 	    	"title": "Scribe Haunt",
 	    	"pic": "img/contact/Caitlin.JPG",
+	    	"email": "scribehaunt13@gmail.com"
 	    },
 	    {
 	    	"name": "Katirin Schrienn",
 	    	"title": "Banker Haunt",
-	    	"pic": "img/contact/Katarina.JPG"
+	    	"pic": "img/contact/Katarina.JPG",
+	    	"email": "bankerhaunt13@gmail.com"
 	    },
 	    {
 	    	"name": "Maggie Widodo & Marilyn McKee",
 	    	"title": "Spirit Haunts",
-	    	"pic": "img/contact/Maggie&Marilyn.JPG"
+	    	"pic": "img/contact/Maggie&Marilyn.JPG",
+	    	"email": "spirithaunt13@gmail.com"
 	    },
 	    {
 	    	"name": "Jacqui Bontke",
 	    	"title": "Singing Haunt",
-	    	"pic": "img/contact/Jacqui.JPG"
+	    	"pic": "img/contact/Jacqui.JPG",
+	    	"email": "singinghaunt13@gmail.com"
 	    },
 	    {
 	    	"name": "Emily Lamon & Maya Brown",
 	    	"title": "Hook 'Em Haunts",
-	    	"pic": "img/contact/Emily&Maya.JPG"
+	    	"pic": "img/contact/Emily&Maya.JPG",
+	    	"email": "hookemhaunts13@gmail.com"
 	    },
 	    {
 	    	"name": "Jessie Neuendorff",
 	    	"title": "Royal Spirit Haunt",
-	    	"pic": "img/contact/Jessie.JPG"
+	    	"pic": "img/contact/Jessie.JPG",
+	    	"email": "royalspirithaunt13@gmail.com"
 	    },
 	    {
 	    	"name": "Michelle Nguyen",
 	    	"title": "Contact Haunt",
-	    	"pic": "img/contact/Michelle.JPG"
+	    	"pic": "img/contact/Michelle.JPG",
+	    	"email": "contacthaunt@gmail.com"
 	    },
 	    {
 	    	"name": "Kayla Jones",
 	    	"title": "Traditions Haunt",
-	    	"pic": "img/contact/Kayla.JPG"
+	    	"pic": "img/contact/Kayla.JPG",
+	    	"email": "traditionshaunt@gmail.com"
 	    },
 	    {
 	    	"name": "Anjali Sethi & Doha Hussien",
 	    	"title": "Service Haunts",
-	    	"pic": "img/contact/Anjali&Doha.JPG"
+	    	"pic": "img/contact/Anjali&Doha.JPG",
+	    	"email": "servicehaunts13@gmail.com"
 	    },
 	    {
 	    	"name": "Ashley McAdamas & Sierra Welch",
 	    	"title": "Special Projects Haunts",
-	    	"pic": "img/contact/Ashley&Sierra.JPG"
+	    	"pic": "img/contact/Ashley&Sierra.JPG",
+	    	"email": "specialprojectshaunts13@gmail.com"
 	    },
 	    {
 	    	"name": "Carmen Collins",
 	    	"title": "Public Relations Haunt",
-	    	"pic": "img/contact/Carmen.JPG"
+	    	"pic": "img/contact/Carmen.JPG",
+	    	"email": "publicrelationshaunt@gmail.com"
 	    }];
 });
